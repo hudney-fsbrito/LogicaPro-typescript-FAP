196399cd3a71f9503663a24e6184f3eba7b5eb77
hudney-jsbrito <hudney.rse@gmail.com>
2024-08-21 10:36:19 -0300

    Classes - Aula inicial de classe

--- aula9/index.ts
+++ aula9/index.ts
@@ -1,1 +1,23 @@
-console.log(`Olá Mundo`);
+class Pessoa {
+    nome: string ;
+    idade: number;
+    sexo: string ;
+    cor?: string;
+
+    constructor(nome:string, idade:number, sexo:string,cor?:string,){
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.cor = cor;
+    };
+
+}
+
+const pessoa1 = new Pessoa('Hudney', 37,'M');
+const pessoa2 = new Pessoa('Pedro', 23, 'M');
+const pessoa3 = new Pessoa('Maria', 32, 'F');
+console.log(pessoa1);
+console.log(pessoa2);
+console.log(pessoa3);
+
+
