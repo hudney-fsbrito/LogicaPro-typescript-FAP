a572c4dab28064132d301daf85565242e7b6ef18
hudney-jsbrito <hudney.rse@gmail.com>
2024-08-28 11:49:18 -0300

    Aula de implementação do js ao html

---
+++ HTML/CssInit/script.js
@@ -0,0 +1,4 @@
+var container = document.querySelector('.container');
+var span = document.createElement('span');
+span.appendChild(document.createTextNode('Texto Bacana renderizado'));
+container.appendChild(span);
---
+++ HTML/CssInit/script.ts
@@ -0,0 +1,5 @@
+const container = document.querySelector('.container')
+const span = document.createElement('span')
+span.appendChild(document.createTextNode('Texto Bacana renderizado'))
+container.appendChild(span)
+
